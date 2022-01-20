51206010a10db2c69dac477bebb65ccaa7426b1d
taiyoslime <t@iyosli.me>
2022-01-20 14:30:15 +0900

    introduce tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge:[
+      "./src/**/*.rs",
+      "./index.html",
+      "./src/**/*.html",
+      "./src/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
