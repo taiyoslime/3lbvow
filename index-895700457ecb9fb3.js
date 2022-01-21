d57067ad680bc828d7ae13ffbbc0204b77bcc99d
taiyoslime <taiyoslime@users.noreply.github.com>
2022-01-21 05:26:51 +0000

    deploy: dc90615764ddcb9b864292e02fe214161c600468

--- index-895700457ecb9fb3.js
+++ index-895700457ecb9fb3.js
@@ -314,7 +314,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-b37c2b8bf3fb906f_bg.wasm', import.meta.url);
+        input = new URL('index-895700457ecb9fb3_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
