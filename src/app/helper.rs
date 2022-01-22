a5405926df6497eaddf737b9d1b88940b8449bad
taiyoslime <t@iyosli.me>
2022-01-22 19:36:12 +0900

    make is_valid_word faster

--- src/app/helper.rs
+++ src/app/helper.rs
@@ -9,18 +9,15 @@ pub fn generate_new_answer() -> String {
 
     let len = constants::WORDLIST.len();
     let index = rng.gen_range(0, len);
-    constants::WORDLIST[index].to_string().to_uppercase()
+    constants::WORDLIST[index].to_string().to_ascii_uppercase()
 }
 
 pub fn is_valid_word(word: &str) -> bool {
-    for s in constants::WORDLIST.iter() {
-        if s.to_uppercase() == word {
-            return true;
-        }
-    }
-    false
+    let word: &str = &(word.to_ascii_lowercase());
+    constants::WORDLIST.binary_search(&word).is_ok()
 }
 
+//TODO
 pub fn generate_new_alphabets_status() -> state::AlphabetsStatus {
     let mut hm = HashMap::new();
 
